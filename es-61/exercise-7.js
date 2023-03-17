8c55a387c513d02458312f6c06e4f931c573d8e9
RobertoFanara <fanararoberto7@gmail.com>
2023-03-17 18:11:25 +0100

    esercizio 19 completato

--- es-61/exercise-7.js
+++ es-61/exercise-7.js
@@ -4,43 +4,43 @@ class Person {
   age;
 
   constructor(firstName, lastName, age) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
   }
 
   // get & set first name
   get firstName() {
-    return this.firstName
+    return this.firstName;
   }
   set firstName(x) {
-    this.firstName = x
+    this.firstName = x;
   }
 
   //get & set last name
   get lastName() {
-    return this.lastName
+    return this.lastName;
   }
   set lastName(x) {
-    this.lastName = x
+    this.lastName = x;
   }
 
   //get & set age
   get age() {
-    return this.age
+    return this.age;
   }
   set age(x) {
-    this.age = x
+    this.age = x;
   }
 
   //get full name
   get fullName() {
-    return `${this.firstName} ${this.lastName} `
+    return `${this.firstName} ${this.lastName}`;
   }
 }
-const person = new Person('Mario', 'Rossi', 25);
+const person = new Person("Mario", "Rossi", 25);
 console.log(person.fullName);
 
-person.firstName = 'Maria';
-person.lastName = 'Verdi';
+person.firstName = "Maria";
+person.lastName = "Verdi";
 console.log(person.fullName);
